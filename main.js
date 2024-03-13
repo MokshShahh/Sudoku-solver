f09ecc48c3d9d22d3c838bfce804dff9d4d1709a
Moksh <moksh13112006@gmail.com>
2024-03-13 20:17:18 +0530

    completed valid number checker function

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sudokuPuzzle = [
+const sudokuPuzzle = [
         [5, 3, 0, 0, 7, 0, 0, 0, 0],
         [6, 0, 0, 1, 9, 5, 0, 0, 0],
         [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -10,6 +9,80 @@ document.addEventListener("DOMContentLoaded", () => {
         [0, 0, 0, 4, 1, 9, 0, 0, 5],
         [0, 0, 0, 0, 8, 0, 0, 7, 9]
     ];
+
+function validnumber(r,c,k){
+  const subgrid_arr=[]
+  row_validation=!sudokuPuzzle[r].includes(k)
+  colomn_arr=[]
+  for(let i=0;i<9;i++){
+    colomn_arr.push(sudokuPuzzle[i][c])
+
+  }
+  colomn_validation=!colomn_arr.includes(k)
+  subgrid_row_start=Math.floor(r/3)*3
+  subgrid_row_end=subgrid_row_start+3
+  subgrid_colomn_start=Math.floor(c/3)*3
+  subgrid_colomn_end=subgrid_colomn_start+3
+  
+  for(i=0;i<subgrid_row_end;i++){
+    for(j=0;j<subgrid_colomn_end;j++){
+      
+        subgrid_arr.push(sudokuPuzzle[i][j])
+
+      
+    }
+  }
+  subgrid_validation=!subgrid_arr.includes(k)
+  
+  return(row_validation && colomn_validation && subgrid_validation)
+  }
+
+
+  
+    
+  
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.clear()
+    validnumber(0,8,2)
+    
+    
+
+
+
+
+    let r=0
+    let c=0
+    // for(r=0;r<9;r++){
+    //   for(c=0;c<9;c++){
+    //     if (sudokuPuzzle[r][c]==0){
+    //       sudokuPuzzle[r][c]=validnumber(r,c)
+    //     }
+    //   }
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     function displayArray() {
         const arrayContainer = document.getElementById('arrayContainer');
         let arrayHTML = 'ARRAY<table>';
@@ -25,7 +98,6 @@ document.addEventListener("DOMContentLoaded", () => {
         arrayHTML += '</table>';
         arrayContainer.innerHTML = arrayHTML;
       }
-    console.log("hello")
       // Call the function to display the array
       displayArray();
     })
